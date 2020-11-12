786ef0a292ce497bc30b0209deff7709fbe1e360
eeyahn <03251025+eeyahn@users.noreply.github.com>
2020-11-12 13:52:47 +0800

    Add project files.

---
+++ hci/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hci
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin(object sender, EventArgs e)
+        {
+            this.Visible = false;
+
+            Form3 mainMenu = new Form3();
+            mainMenu.Show();
+            MessageBox.Show("Login Successful");
+
+            //MessageBox.Show("Login Failed");
+        }
+
+        private void btnCancel(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void lnkSignup(object sender, EventArgs e)
+        {
+            this.Visible = false;
+
+            Form2 signUp = new Form2();
+            signUp.Show();
+
+        }
+
+    }
+}
---
+++ hci/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hci
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sign Up Successful");
+            this.Visible = false;
+            Form1 loginForm = new Form1();
+            loginForm.Show();
+        }
+        private void btnCancel(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
