66380e06abfd9a84263653c223f7ca3dea767e07
eeyahn <03251025+eeyahn@users.noreply.github.com>
2020-11-12 15:56:32 +0800

    Di ko na tinanggal yung mga .dll files na ininclude sa vid XD

--- hci/Form1.cs
+++ hci/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace hci
 {
@@ -29,13 +30,38 @@ namespace hci
 
         private void btnLogin(object sender, EventArgs e)
         {
-            this.Visible = false;
+            if (textBox1.Text.Trim() == "" && textBox2.Text.Trim() == "")
+            {
+                MessageBox.Show("Empty Fields");
+            }
+            else
+            {
+                string query = "SELECT * FROM UserInformation WHERE UserName= @textBox1 AND Password= @textBox2";
+                SQLiteConnection Con = new SQLiteConnection("Data Source=Database.db;Version=3;");
+                Con.Open();
+                SQLiteCommand cmd = new SQLiteCommand(query, Con);
+                cmd.Parameters.AddWithValue("@textBox1",textBox1.Text);
+                cmd.Parameters.AddWithValue("@textBox2", textBox2.Text);
+                SQLiteDataAdapter DA = new SQLiteDataAdapter(cmd);
+                DataTable DT = new DataTable();
+                DA.Fill(DT);
 
-            Form3 mainMenu = new Form3();
-            mainMenu.Show();
-            MessageBox.Show("Login Successful");
+                if(DT.Rows.Count > 0)
+                {
+                    this.Visible = false;
+                    Form3 mainMenu = new Form3();
+                    mainMenu.Show();
+                    MessageBox.Show("Login Successful");
+                }
+                else
+                {
+                    MessageBox.Show("Login Failed");
+                }
 
-            //MessageBox.Show("Login Failed");
+                
+            }
+
+            
         }
 
         private void btnCancel(object sender, EventArgs e)
